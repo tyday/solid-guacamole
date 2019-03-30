2c29077692be98f28aff8529aa3aa129e48415bf
Ty Day <tyrday@gmail.com>
2019-03-30 19:52:12 -0400

    add authentication

---
+++ Section-04/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'testkey'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) #/auth
+
+items = []
+
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item':item}, 200 if item else 404
+    
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        data = request.get_json()
+        item = {'name':name, 'price':data['price']}
+        items.append(item)
+        return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'item': items}
+
+class ItemList(Resource):
+    def get(self):
+        return {'item': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
