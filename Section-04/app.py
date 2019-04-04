c5014cbb2c8b07f007bad208714e365744fe7810
Ty Day <tyrday@gmail.com>
2019-04-04 16:24:01 -0400

    implement sql db

--- Section-04/app.py
+++ Section-04/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'testkey'
@@ -57,6 +58,7 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE users (id int, username text, password text)"
+cursor.execute(create_table)
+
+user = (1, 'jose', 'asdf')
+insert_query = "INSERT INTO users VALUES(?,?,?)"
+cursor.execute(insert_query,user)
+
+connection.commit()
+
+connection.close()
