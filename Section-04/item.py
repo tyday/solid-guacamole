f71d83c2185a5d520f41cce391d5af1cd925de70
Ty Day <tyrday@gmail.com>
2019-04-06 19:11:16 -0400

    moved queryDB into Item class

--- Section-04/item.py
+++ Section-04/item.py
@@ -2,18 +2,6 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 
-def queryDB(query,qry_variables=None):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-    
-    if qry_variables:
-        result = cursor.execute(query, qry_variables)
-    else:
-        result = cursor.execute(query)
-    rows = result.fetchall()
-    connection.commit()
-    connection.close()
-    return rows
 
 
 
@@ -26,10 +14,24 @@ class Item(Resource):
     @jwt_required()
     def get(self, name):
         item = self.find_by_name(name)
+        print(item)
         if item:
-            return {'item': {'name':row[0], 'price':row[1]}}
+            return item
         else:
             return {'message': 'Item not found'}, 404
+    @classmethod
+    def queryDB(self, query, qry_variables=None):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        if qry_variables:
+            result = cursor.execute(query, qry_variables)
+        else:
+            result = cursor.execute(query)
+        rows = result.fetchall()
+        connection.commit()
+        connection.close()
+        return rows
 
     @classmethod
     def find_by_name(cls, name):
@@ -54,19 +56,31 @@ class Item(Resource):
         item = {'name':name, 'price':data['price']}
 
         query = "INSERT INTO items VALUES (?,?)"
-        queryDB(query,(item['name'], item['price']))
+        self.queryDB(query,(item['name'], item['price']))
 
         return item, 201
     
     def delete(self, name):
         if Item.find_by_name(name):
             query = "DELETE FROM items WHERE name=?"
-            queryDB(query,(name,))
+            self.queryDB(query,(name,))
             return {'message': 'Item deleted'}
         return {'message':'Item not found.'}, 404
 
     def put(self, name):
         data = Item.parser.parse_args()
+        print(data)
+        if Item.find_by_name(name):
+            query = "UPDATE items SET price=? WHERE name=?"
+            self.queryDB(query,(data['price'], name))
+            item = {'name':name,'price':data['price']}
+            return {'item': item}
+        else:
+            query = "INSERT INTO items VALUES (?,?)"
+            self.queryDB(query,(name,data['price']))
+            item = {'name':name,'price':data['price']}
+            return {'item':item}, 201
+        
 
         item = next(filter(lambda x: x['name']== name, items), None)
         if item is None:
