9277e81e6b04a45c4930153f4ad0870a36a798ee
Ty Day <tyrday@gmail.com>
2019-04-08 14:45:42 -0400

    hope this doesn't fuck things up

--- Section-06/app.py
+++ Section-06/app.py
@@ -7,6 +7,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
@@ -17,5 +18,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
+    from db import db
+    db.init_app(app)
     app.run(debug=True)  # important to mention debug=True
